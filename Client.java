04d7b203c719ca00b58d10f9fc23567963785add
Kikusohame <blechnereddie@utlook.de>
2020-11-22 20:43:11 +0100

    meep

--- Client.java
+++ Client.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
-
 public class Client {
     public static void main(String[] args) throws IOException{
         Socket s = new Socket("localhost", 4999);
--- Server.java
+++ Server.java
@@ -2,22 +2,37 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    public static void main(String[] args) throws IOException{
+    public void run() {
+
         ServerSocket ss = new ServerSocket(4999);
-        Socket s = ss.accept();
+        while (true) {
+            try {
+                
+                Socket s = ss.accept();
 
-        System.out.println("client connected");
+                System.out.println("client connected");
 
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
-        BufferedReader bf = new BufferedReader(in);
+                InputStreamReader in = new InputStreamReader(s.getInputStream());
+                BufferedReader bf = new BufferedReader(in);
 
-        String str = bf.readLine();
-        System.out.println(("client: "+ str));
+                String str = bf.readLine();
+                System.out.println(("client: " + str));
 
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
-        pr.println("yes");
-        pr.flush();
+                PrintWriter pr = new PrintWriter(s.getOutputStream());
+                pr.println("yes");
+                pr.flush();
 
-        ss.close();
+            } catch (IOException e) {
+                // TODO: handle exception
+            }
+            finally{
+
+            }
+
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
     }
 }
