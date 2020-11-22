be2b38d09deba7f369edb5119d793ce0f4e8f5e6
RR <rene.roesch.99mail.com#>
2020-11-22 21:05:31 +0100

    setup in different functions

--- Server.java
+++ Server.java
@@ -2,37 +2,58 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
+
+    private ServerSocket ss;
+    private int portNumber;
+    private Socket connection;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    public void main(String[] args) throws IOException {
+        run();
+    }
+
     public void run() {
+        try {
+            ss = new ServerSocket(portNumber, 100);
 
-        ServerSocket ss = new ServerSocket(4999);
-        while (true) {
-            try {
-                
-                Socket s = ss.accept();
-
-                System.out.println("client connected");
-
-                InputStreamReader in = new InputStreamReader(s.getInputStream());
-                BufferedReader bf = new BufferedReader(in);
-
-                String str = bf.readLine();
-                System.out.println(("client: " + str));
-
-                PrintWriter pr = new PrintWriter(s.getOutputStream());
-                pr.println("yes");
-                pr.flush();
-
-            } catch (IOException e) {
-                // TODO: handle exception
+            while (true) {
+                try {
+                    connect();
+                    setupStreams();
+                    chat();
+                } catch (EOFException eofException) {
+                    System.out.println("\n Server ended the connection! ");
+                } finally {
+                    close();
+                }
             }
-            finally{
-
-            }
-
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    private void connect() throws IOException {
+        System.out.println("Waiting for someone to connect... \n");
+        connection = ss.accept();
+        System.out.println("Now connected to " + connection.getInetAddress().getHostName());
+    }
 
+    private void setupStreams() throws IOException {
+        InputStreamReader in = new InputStreamReader(connection.getInputStream());
+        input = new BufferedReader(in);
+        output = new PrintWriter(connection.getOutputStream());
+        output.flush();
+    }
+
+    private void chat(){
+        // TODO: create functions to send messages
+    }
+
+    private void close() throws IOException {
+        System.out.println("\n Closing connections... \n");
+        input.close();
+        output.close();
+        connection.close();
     }
 }
