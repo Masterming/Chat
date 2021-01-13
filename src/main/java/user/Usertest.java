eab1047b2b008cf277a035384d99dd1445a41711
Kikusohame <blechnereddie@utlook.de>
2021-01-13 23:42:04 +0100

    parser in arbeit,meep

--- src/main/java/user/Usertest.java
+++ src/main/java/user/Usertest.java
@@ -9,29 +9,5 @@ public class Usertest {
         User c = new User("127.0.0.1", 4999);
         c.run();
 
-        UserGUI test2 = new UserGUI();
-
-        // neue Nachricht
-        test2.addMessage("test123");
-        for (int i = 0; i < 30; i++) {
-            test2.addMessage("" + i + "\n");
-        }
-
-        test2.setServerInformation(4999, "testIP");
-        test2.setUserInformation("Tom");
-
-        String[] rooms = new String[3];
-
-        for (int i = 0; i < 3; i++) {
-            rooms[i] = "Raum " + i;
-        }
-        test2.setRooms(rooms);
-
-        String[] user = new String[3];
-
-        for (int i = 0; i < 3; i++) {
-            user[i] = "Nutzer " + i;
-        }
-        test2.setUsers(user);
     }
 }
---
+++ src/main/parser/Jsonparser.java
@@ -0,0 +1,9 @@
+public class Jsonparser {
+    public int id;
+    public String content;
+
+    public jsonparser(int _id, String _content){
+        id = _id;
+        content = _content;
+    }
+}
